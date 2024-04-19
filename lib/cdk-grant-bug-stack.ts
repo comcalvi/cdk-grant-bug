ebb8a70b14449e24e09f0e469f8444602d0518ea
Calvin Combs <comcalvi@amazon.com>
2024-04-19 09:17:29 -0700

    cleanup

--- lib/cdk-grant-bug-stack.ts
+++ lib/cdk-grant-bug-stack.ts
@@ -27,6 +27,7 @@ export class CdkGrantBug extends ecs.FargateService {
   }
 }
 
+/*
 export class CdkGrantBugGoodScopes extends ecs.FargateService {
   constructor(scope: Construct, id: string) {
     super(scope, id, {
@@ -48,7 +49,9 @@ export class CdkGrantBugGoodScopes extends ecs.FargateService {
     queue.grantConsumeMessages(this.taskDefinition.taskRole);
   }
 }
+*/
 
+/*
 export class CdkGrantBugComponent extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
@@ -71,55 +74,12 @@ export class CdkGrantBugComponent extends Construct {
     queue.grantConsumeMessages(fargateService.taskDefinition.taskRole);
   }
 }
-
-export class QueueConstruct extends Construct {
-  public queue1: sqs.IQueue;
-  public queue2: sqs.IQueue;
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-
-    this.queue1 = new sqs.Queue(this, 'Queue1');
-    this.queue2 = new sqs.Queue(this, 'Queue2');
-  }
-}
-
-export class RoleConstruct extends Construct {
-  public role: iam.IRole;
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-
-    this.role = new iam.Role(this, 'role', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-    });
-  }
-}
-
-export class L3Construct extends Construct {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-    const A = new Construct(scope, 'A');
-    const B = new Construct(scope, 'B');
-
-    const C = new QueueConstruct(A, 'C');
-    const D = new QueueConstruct(A, 'D');
-
-    const E = new RoleConstruct(B, 'E');
-    const F = new RoleConstruct(B, 'F');
-
-    //C.node.addDependency(E);
-    C.queue1.grantConsumeMessages(E.role);
-    //C.queue1.node.addDependency(D.queue1);
-  }
-}
+*/
 
 export class CdkGrantBugStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    //new CdkGrantBugGoodScopes(this, 'CdkGrantBug');
     new CdkGrantBug(this, 'CdkGrantBug');
-    //new CdkGrantBugComponent(this, 'CdkGrantBug');
-
-    //new L3Construct(this, 'L3');
   }
 }
